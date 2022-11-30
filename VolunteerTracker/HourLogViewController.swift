77833059a3f650515faccc73f0a4dd775ad8350e
erubright37 <emrubright@student.fullsail.edu>
2022-11-30 13:46:12 -0600

    Add NewLogScreen and UI Layout

---
+++ VolunteerTracker/HourLogViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HourLogViewController.swift
+//  VolunteerTracker
+//
+//  Created by Emily Rubright on 11/30/22.
+//
+
+import UIKit
+
+class HourLogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
