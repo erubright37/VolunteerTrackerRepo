7f0c570c5857ae7277e6e8b210788eb8200e1b68
erubright37 <emrubright@student.fullsail.edu>
2022-12-09 13:12:38 -0600

    Categories Screen UI Categories Screen Add Category

--- VolunteerTracker/CategoriesViewController.swift
+++ VolunteerTracker/CategoriesViewController.swift
@@ -7,17 +7,58 @@
 
 import UIKit
 
-class CategoriesViewController: UIViewController {
+class CategoriesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var categories = [String]()
+    
+    @IBOutlet weak var catTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        catTableView.delegate = self
+        catTableView.dataSource = self
     }
     
+    @IBAction func addCategoryClick(_ sender: Any) {
+        let alert = UIAlertController(title: "Add Category", message: "Type the category you'd like to add", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Enter Skill"
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { (action:UIAlertAction) in
+            guard let textField =  alert.textFields?.first?.text
+            else {
+                return
+            }
+            self.categories.append(textField)
+            self.catTableView.reloadData()
+        }))
+        present(alert, animated: true)
+        
+    }
+    
+    // MARK: - Table view data source
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cat_cell", for: indexPath)
+        
+        // Configure
+        cell.textLabel?.text = categories[indexPath.row]
+        
+        return cell
+    }
+
+    
     /*
     // MARK: - Navigation
 
